10734b97a12ce774d7d18421b1c82fe7420cecc8
LaszloR1 <r1titanhu@gmail.com>
2023-12-11 17:01:36 +0100

    Added FetchHeaders functionality to the Http module

--- js/Http.js
+++ js/Http.js
@@ -24,6 +24,17 @@ export class Http {
 
     /**
      * @param {string} url
+     * @return {Promise<Headers>}
+     */
+    static async FetchHeaders(url) {
+        const options = {...Http.options, ...{method: "HEAD"}};
+        const result = await fetch(url, options);
+
+        return result.headers;
+    }
+
+    /**
+     * @param {string} url
      * @return {Promise<Int8Array>}
      */
     static async FetchData(url) {
