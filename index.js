2f06eea4b80d11323f4f9bfc878a708b444f7556
LaszloR1 <r1titanhu@gmail.com>
2023-12-11 17:03:51 +0100

    Schedule implemented, logging

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import {News} from "./js/News.js";
+import {Schedule} from "./js/Schedule.js";
 
+console.log("Fetching News...");
 await News.Fetch();
+
+console.log("Fetching schedule...");
+await Schedule.Fetch();
+
+console.log("Done!");
